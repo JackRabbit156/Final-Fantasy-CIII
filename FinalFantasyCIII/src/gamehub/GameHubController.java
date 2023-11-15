be5812b5ab146d456c1f5e39dc38d4bda6bb2949
11436766 <nielstorbenrode@auf.bundeswehr.de>
2023-11-15 18:29:41 +0100

    DEV: Erstellung Menue untergruppen und co

--- FinalFantasyCIII/src/gamehub/GameHubController.java
+++ FinalFantasyCIII/src/gamehub/GameHubController.java
@@ -9,6 +9,8 @@ import party.PartyController;
 import party.PartyStatusController;
 import statistik.StatistikController;
 
+import java.util.Scanner;
+
 public class GameHubController {
     private GameController gameController;
     private PartyController partyController;
@@ -18,8 +20,11 @@ public class GameHubController {
     private TrainerController trainer;
     private PartyStatusController partystatus;
     private StatistikController statistik;
+    private Scanner einleser;
+    private int selectedOption = 0;
 
-    public GameHubController(GameController gameController, PartyController partyController, HaendlerController haendler, SchmiedeController schmiede, TaverneController taverne, TrainerController trainer, PartyStatusController partystatus, StatistikController statistik) {
+
+    public GameHubController(GameController gameController, PartyController partyController, HaendlerController haendler, SchmiedeController schmiede, TaverneController taverne, TrainerController trainer, PartyStatusController partystatus, StatistikController statistik, Scanner einleser) {
         this.gameController = gameController;
         this.partyController = partyController;
         this.haendler = haendler;
@@ -28,6 +33,11 @@ public class GameHubController {
         this.trainer = trainer;
         this.partystatus = partystatus;
         this.statistik = statistik;
+        this.einleser = einleser;
+    }
+
+    public Scanner getEinleser() {
+        return einleser;
     }
 
     public PartyStatusController getPartystatus() {
@@ -61,4 +71,62 @@ public class GameHubController {
     public StatistikController getStatistik() {
         return statistik;
     }
-}
+
+    private void hubAnzeigen() {
+        String[] menuOption = {"Händler", "Schmiede", "Partystatus Einsehen", "Ausruhen", "Kampf starten", "DEVELOP: Spiel beenden"};
+
+
+        while (true) {
+            System.out.println(Farbauswahl.RED + "Select an option:" + Farbauswahl.RESET);
+
+            for (int i = 0; i < menuOption.length; i++) {
+                if (i == selectedOption) {
+                    // Hier wird der Bereich gezeichnet wo man sich gerade befindet und in Gelb dargestellt
+                    System.out.println(Farbauswahl.YELLOW + ">> " + menuOption[i] + Farbauswahl.RESET);
+                } else {
+                    // Hier wird alles gezeichnet was momentan nicht ausgewaehlt ist.
+                    System.out.println(Farbauswahl.GREEN + "   " + menuOption[i] + Farbauswahl.RESET);
+                }
+            }
+
+            char userInput = einleser.next().charAt(0);
+
+            switch (userInput) {
+                case 'w':
+                    navigateMenu(-1, menuOption.length);
+                    break;
+                case 's':
+                    navigateMenu(1, menuOption.length);
+                    break;
+                case 'e':
+                    executeSelectedOption(selectedOption, menuOption);
+                    break;
+                default:
+                    System.out.println("Invalid input. Use 'w' to move up, 's' to move down, and e to select.");
+                    break;
+            }
+        }
+    }
+
+    private void navigateMenu(int direction, int menuLength) {
+        this.selectedOption = (selectedOption + direction + menuLength) % menuLength;
+    }
+
+    private void executeSelectedOption(int selectedOption, String[] menuOptions) {
+        System.out.println("Executing: " + menuOptions[selectedOption]);
+        // Hier kann man einfügen, was passieren soll
+        switch (selectedOption) {
+//        case 0:
+//            newGame();
+//            break;
+//        case 1:
+//            load();
+//            break;
+            case 6:
+                System.exit(0);
+                break;
+        }
+        // anstelle eines Switch Case könnte man auch einfach ein IF nehmen.
+    }
+
+
