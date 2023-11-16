4361b9bd47b6c98417f749dcf2cf2ceff744382e
TangoMike <Thomas1Maass@Bundeswehr.org>
2023-11-16 12:58:48 +0100

    +Trainer +TrainerController

--- FinalFantasyCIII/src/gamehub/trainer/TrainerController.java
+++ FinalFantasyCIII/src/gamehub/trainer/TrainerController.java
@@ -1,4 +1,59 @@
 package gamehub.trainer;
 
+import gamehub.trainer.faehigkeiten.Faehigkeit;
+import party.PartyController;
+
+/**
+ * @author Thomas Maass
+ * @since Initiale Befuellung des Controllers mit den Variablen und Methoden.
+ */
 public class TrainerController {
+
+    private Trainer trainer;
+
+    // Methoden
+    public void trainerAnzeigen(PartyController partyController) {
+
+        System.out.println("Willkommen beim Trainer !");
+        System.out.println("Der Trainer ist insolvent.");
+
+    }
+
+    private void faehigkeitenZuruecksetzen() {
+
+    }
+
+    private void faehigkeitenLernen(Faehigkeit faehigkeit) {
+    }
+
+    private void maxGesundheitsPunkteVerbessern(int gesundheitsPunkte) {
+    }
+
+    private void maxManaPunkteVerbessern(int manaPunkte) {
+    }
+
+    private void physischeAttackeVerbessern(int angriffsPunkte) {
+    }
+
+    private void magischeAttackeVerbessern(int magischeAtk) {
+    }
+
+    private void genauigkeitVerbessern(int genauigkeitsWert) {
+    }
+
+    private void verteidigungVerbessern(int verteidigungsWert) {
+    }
+
+    private void resistenzVerbessern(int resistenzWert) {
+    }
+
+    private void bewegklichkeitVerbessern(int beweglichkeitsWert) {
+    }
+
+    private void klasseAendern(Klasse klasse) {
+    }
+
+    private void spezialisierungAendern(Klasse klasse) {
+    } //@TODO : Vllt. Aenderung der Bezeichnung um zwischen der Klasse (hat jeder) und der spezialisierung (ab gewissen lvl. mgl) !!! /TM
+
 }
