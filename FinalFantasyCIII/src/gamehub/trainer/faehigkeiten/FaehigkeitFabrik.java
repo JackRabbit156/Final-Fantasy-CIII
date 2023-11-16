57e5d0b53055c6b22448f542f60113eeb2ce215e
11777914 <oliverebert@auf.bundeswehr.de>
2023-11-16 18:36:52 +0100

    Faehigkeiten für Spezialisierung erstellt. Spezialisierung können hinzugefuegt oder fuer NPC direkt neu erstellt werden.

--- FinalFantasyCIII/src/gamehub/trainer/faehigkeiten/FaehigkeitFabrik.java
+++ FinalFantasyCIII/src/gamehub/trainer/faehigkeiten/FaehigkeitFabrik.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 
 public class FaehigkeitFabrik {
 
-    public static Faehigkeit[] erstelleFaehigkeitFuer(Charakter charakter){
+    public static ArrayList<Faehigkeit> erstelleFaehigkeitFuer(Charakter charakter) {
         String klassenNamen = charakter.getKlasse().getBezeichnung();
-        int faehigkeitsPunkte = charakter.getLevel(); //TODO: level zu skill-Punkte Ratio
+        //TODO: level zu skill-Punkte Ratio
+        int faehigkeitsPunkte = charakter.getLevel();
 
-        Faehigkeit[] returnFaehigkeiten;
-        switch(klassenNamen){
+        ArrayList<Faehigkeit> returnFaehigkeiten;
+        switch (klassenNamen) {
             case "PDD":
                 returnFaehigkeiten = NeueFaehigkeiten.neuePDDFaehigkeiten();
                 break;
@@ -24,25 +25,90 @@ public class FaehigkeitFabrik {
             case "HLR":
                 returnFaehigkeiten = NeueFaehigkeiten.neueHLRFaehigkeiten();
                 break;
+            case "Berserker":
+                returnFaehigkeiten = NeueFaehigkeiten.neuePDDFaehigkeiten();
+                returnFaehigkeiten.addAll(NeueFaehigkeiten.neueBerserkerFaehigkeit());
+                break;
+            case "Schurke":
+                returnFaehigkeiten = NeueFaehigkeiten.neuePDDFaehigkeiten();
+                returnFaehigkeiten.addAll(NeueFaehigkeiten.neueSchurkeFaehigkeit());
+                break;
+            case "Feuermagier":
+                returnFaehigkeiten = NeueFaehigkeiten.neueMDDFaehigkeiten();
+                returnFaehigkeiten.addAll(NeueFaehigkeiten.neueFeuermagierFaehigkeit());
+                break;
+            case "Eismagier":
+                returnFaehigkeiten = NeueFaehigkeiten.neueMDDFaehigkeiten();
+                returnFaehigkeiten.addAll(NeueFaehigkeiten.neueEismagierFaehigkeit());
+                break;
+            case "Rabauke":
+                returnFaehigkeiten = NeueFaehigkeiten.neueTNKFaehigkeiten();
+                returnFaehigkeiten.addAll(NeueFaehigkeiten.neueRabaukeFaehigkeit());
+                break;
+            case "Paladin":
+                returnFaehigkeiten = NeueFaehigkeiten.neueTNKFaehigkeiten();
+                returnFaehigkeiten.addAll(NeueFaehigkeiten.neuePaladinFaehigkeit());
+                break;
+            case "Priester":
+                returnFaehigkeiten = NeueFaehigkeiten.neueHLRFaehigkeiten();
+                returnFaehigkeiten.addAll(NeueFaehigkeiten.neuePriesterFaehigkeit());
+                break;
+            case "Sanmaus":
+                returnFaehigkeiten = NeueFaehigkeiten.neueHLRFaehigkeiten();
+                returnFaehigkeiten.addAll(NeueFaehigkeiten.neueSanmausFaehigkeit());
+                break;
             default:
                 throw new IllegalArgumentException("Keinen solchen Klassennamen gefunden!");
         }
-        if(faehigkeitsPunkte > 1){
+        if (faehigkeitsPunkte > 1) {
             faehigkeitenVerteilen(returnFaehigkeiten, faehigkeitsPunkte);
         }
         return returnFaehigkeiten;
     }
 
-    private static void faehigkeitenVerteilen(Faehigkeit[] faehigkeits, int level){
+    public static void spezialisierungsFaehigkeitHinzufuegen(Charakter charakter){
+        String klassenNamen = charakter.getKlasse().getBezeichnung();
+
+        switch (klassenNamen) {
+            case "Berserker":
+                charakter.getFaehigkeiten().addAll(NeueFaehigkeiten.neueBerserkerFaehigkeit());
+                break;
+            case "Schurke":
+                charakter.getFaehigkeiten().addAll(NeueFaehigkeiten.neueSchurkeFaehigkeit());
+                break;
+            case "Feuermagier":
+                charakter.getFaehigkeiten().addAll(NeueFaehigkeiten.neueFeuermagierFaehigkeit());
+                break;
+            case "Eismagier":
+                charakter.getFaehigkeiten().addAll(NeueFaehigkeiten.neueEismagierFaehigkeit());
+                break;
+            case "Rabauke":
+                charakter.getFaehigkeiten().addAll(NeueFaehigkeiten.neueRabaukeFaehigkeit());
+                break;
+            case "Paladin":
+                charakter.getFaehigkeiten().addAll(NeueFaehigkeiten.neuePaladinFaehigkeit());
+                break;
+            case "Priester":
+                charakter.getFaehigkeiten().addAll(NeueFaehigkeiten.neuePriesterFaehigkeit());
+                break;
+            case "Sanmaus":
+                charakter.getFaehigkeiten().addAll(NeueFaehigkeiten.neueSanmausFaehigkeit());
+                break;
+            default:
+                System.err.println("FaehigkeitFabrik: Keinen solchen Klassennamen gefunden!");
+        }
+    }
+
+    private static void faehigkeitenVerteilen(ArrayList<Faehigkeit> faehigkeits, int level) {
         ArrayList<Faehigkeit> moeglicheFaehigkeiten = new ArrayList<>();
-        for (Faehigkeit faehigkeit : faehigkeits){
-            if(faehigkeit.getLevelAnforderung() >= level){
+        for (Faehigkeit faehigkeit : faehigkeits) {
+            if (faehigkeit.getLevelAnforderung() >= level) {
                 moeglicheFaehigkeiten.add(faehigkeit);
             }
         }
         int anzahlMoeglicherFaehigkeiten = moeglicheFaehigkeiten.size();
         for (int i = 0; i < level; i++) {
-            Faehigkeit zufaelligeFaehigkeit = moeglicheFaehigkeiten.get((int)(Math.random()*anzahlMoeglicherFaehigkeiten));
+            Faehigkeit zufaelligeFaehigkeit = moeglicheFaehigkeiten.get((int) (Math.random() * anzahlMoeglicherFaehigkeiten));
             Faehigkeit.faehigkeitAufwerten(zufaelligeFaehigkeit);
         }
     }
--- FinalFantasyCIII/src/gamehub/trainer/faehigkeiten/NeueFaehigkeiten.java
+++ FinalFantasyCIII/src/gamehub/trainer/faehigkeiten/NeueFaehigkeiten.java
@@ -1,9 +1,13 @@
 package gamehub.trainer.faehigkeiten;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//TODO: JAVA DOC ERGAENZEN!
 public class NeueFaehigkeiten {
 
-    public static Faehigkeit[] neuePDDFaehigkeiten() {
-        return new Faehigkeit[]{
+    public static ArrayList<Faehigkeit> neuePDDFaehigkeiten() {
+        return new ArrayList<>(Arrays.asList(
                 //Standard
                 new Faehigkeit("Standard physischer Angriff",
                         "Der Standardangriff dieser Klasse.",
@@ -43,11 +47,11 @@ public class NeueFaehigkeiten {
                         "gesundheitsPunkte"
                 )
                 //TODO: weitere Skills PDD implementieren
-        };
+        ));
     }
 
-    public static Faehigkeit[] neueMDDFaehigkeiten() {
-        return new Faehigkeit[]{
+    public static ArrayList<Faehigkeit> neueMDDFaehigkeiten() {
+        return new ArrayList<>(Arrays.asList(
                 //Standard
                 new Faehigkeit("Standard magischer Angriff",
                         "Der Standardangriff dieser Klasse.",
@@ -88,11 +92,11 @@ public class NeueFaehigkeiten {
                         "gesundheitsPunkte"
                 )
                 //TODO: weitere Skills MDD implementieren
-        };
+        ));
     }
 
-    public static Faehigkeit[] neueTNKFaehigkeiten() {
-        return new Faehigkeit[]{
+    public static ArrayList<Faehigkeit> neueTNKFaehigkeiten() {
+        return new ArrayList<>(Arrays.asList(
                 //Standard
                 new Faehigkeit("Standard Verteidiger-Angriff",
                         "Der Standardangriff dieser Klasse.",
@@ -133,11 +137,11 @@ public class NeueFaehigkeiten {
                         "manaPunkte"
                 )
                 //TODO: weitere Skills PDD implementieren
-        };
+        ));
     }
 
-    public static Faehigkeit[] neueHLRFaehigkeiten() {
-        return new Faehigkeit[]{
+    public static ArrayList<Faehigkeit> neueHLRFaehigkeiten() {
+        return new ArrayList<>(Arrays.asList(
                 //Standard
                 new Faehigkeit("Standard Heilung",
                         "Der Standardheilung dieser Klasse.",
@@ -177,6 +181,159 @@ public class NeueFaehigkeiten {
                         1,
                         "gesundheitsPunkte")
                 //TODO: weitere Skills HLR implementieren
-        };
+        ));
+    }
+
+    public static ArrayList<Faehigkeit> neueBerserkerFaehigkeit() {
+        return new ArrayList<>(Arrays.asList(
+                //Berserker Spezial
+                new Faehigkeit("Roter Nebel",
+                        "Der Spezialangriff dieser Klasse: Greift einen zufälligen Gegner mit großem Schaden an - verliert jedoch selber Lebenspunkte.",
+                        50,
+                        1,
+                        10,
+                        false,
+                        50,
+                        1,
+                        1,
+                        1,
+                        "gesundheitsPunkte"
+                )
+
+                //TODO: weitere Skills Berserker implementieren
+        ));
+    }
+
+    public static ArrayList<Faehigkeit> neueSchurkeFaehigkeit() {
+        return new ArrayList<>(Arrays.asList(
+                //Schurke Spezial
+                new Faehigkeit("Finte",
+                        "Der Spezialangriff dieser Klasse: Ein zufaelliger Gegner greift einen anderen Gegner mit einem seiner Faehigkeiten an.",
+                        50,
+                        1,
+                        10,
+                        false,
+                        1,
+                        1,
+                        1,
+                        1,
+                        "werIstDranAttribut"
+                )
+                //TODO: weitere Skills Schurke implementieren
+        ));
+    }
+
+    public static ArrayList<Faehigkeit> neueFeuermagierFaehigkeit() {
+        return new ArrayList<>(Arrays.asList(
+                //Feuermagier Spezial
+                new Faehigkeit("Feuersturm",
+                        "Der Spezialangriff dieser Klasse: grossse Feuerball, Junge!",
+                        50,
+                        1,
+                        10,
+                        false,
+                        40,
+                        1,
+                        1,
+                        1,
+                        "gesundheitsPunkte"
+                )
+                //TODO: weitere Skills Feuermagier implementieren
+        ));
+    }
+
+    public static ArrayList<Faehigkeit> neueEismagierFaehigkeit() {
+        return new ArrayList<>(Arrays.asList(
+                //Eismagier Spezial
+                new Faehigkeit("Eissturm",
+                        "Der Spezialangriff dieser Klasse: Ein Gegner muss im naechstem Zug aussetzen.",
+                        50,
+                        1,
+                        10,
+                        false,
+                        0,
+                        1,
+                        1,
+                        1,
+                        "werIstDranAttribut"
+                )
+                //TODO: weitere Skills Eismagier implementieren
+        ));
+    }
+
+    public static ArrayList<Faehigkeit> neueRabaukeFaehigkeit() {
+        return new ArrayList<>(Arrays.asList(
+                //Rabauke Spezial
+                new Faehigkeit("Ausweichen",
+                        "Der Spezialangriff dieser Klasse: Der erste Schlag des Gegners verfehlt das Ziel.",
+                        50,
+                        1,
+                        10,
+                        false,
+                        0,
+                        1,
+                        1,
+                        1,
+                        "werIstDranAttribut"
+                )
+                //TODO: weitere Skills Rabauke implementieren
+        ));
+    }
+
+    public static ArrayList<Faehigkeit> neuePaladinFaehigkeit() {
+        return new ArrayList<>(Arrays.asList(
+                //Paladin Spezial
+                new Faehigkeit("Fels in der Brandung",
+                        "Der Spezialangriff dieser Klasse: Erhoeht die Gesundheitspunkte um einen Brocken.",
+                        50,
+                        1,
+                        10,
+                        true,
+                        80,
+                        1,
+                        1,
+                        1,
+                        "gesundheitsPunkte"
+                )
+                //TODO: weitere Skills Paladin implementieren
+        ));
+    }
+
+    public static ArrayList<Faehigkeit> neuePriesterFaehigkeit() {
+        return new ArrayList<>(Arrays.asList(
+                //Priester Spezial
+                new Faehigkeit("Bekehren",
+                        "Der Spezialangriff dieser Klasse: Ein Gegner läuft zum eigenen Team über - wenn Platz ist",
+                        50,
+                        1,
+                        10,
+                        true,
+                        0,
+                        1,
+                        1,
+                        1,
+                        "TeamZugehoerigkeit"
+                )
+                //TODO: weitere Skills Priester implementieren
+        ));
+    }
+
+    public static ArrayList<Faehigkeit> neueSanmausFaehigkeit() {
+        return new ArrayList<>(Arrays.asList(
+                //SanMaus Spezial
+                new Faehigkeit("Neukrank!",
+                        "Der Spezialangriff dieser Klasse: Teammitglied wird wiederbelebt.",
+                        50,
+                        1,
+                        10,
+                        true,
+                        0,
+                        1,
+                        1,
+                        1,
+                        "gesundheitsPunkte"
+                )
+                //TODO: weitere Skills SanMaus implementieren
+        ));
     }
 }
