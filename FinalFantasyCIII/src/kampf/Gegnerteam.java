9757afdbacd41060ba8a3a0632bf80224f4f2fbb
TangoMike <Thomas1Maass@Bundeswehr.org>
2023-11-16 12:57:50 +0100

    +kampfInitialisierung +Gegnerteam

--- FinalFantasyCIII/src/kampf/Gegnerteam.java
+++ FinalFantasyCIII/src/kampf/Gegnerteam.java
@@ -1,4 +1,22 @@
 package kampf;
 
+import charakter.controller.FeindController;
+import charakter.model.Feind;
+
+import java.util.Random;
+/**
+ * @author Thomas Maass
+ * @param BSPTEXT - dokumentiert ein an eine Methode übergebenes Argument.
+ * @return
+ * @since BSPTEXT - dient zur Hervorhebung der Einführung in den Code, also seit wann (z.B. Versionsnummer) ein bestimmtes Feature verfügbar ist.
+ */
 public class Gegnerteam {
+    // Gegnerteam erstellen lassen
+    // Stellvertretend für die Klasse
+    // @TODO nach Erstellung der zentralen Random Klasse Arraygroesse zufällig bestimmen lassen (max 4)
+    //private Feind[] feinde = new Feind[4];
+
+    }
+
+
 }
