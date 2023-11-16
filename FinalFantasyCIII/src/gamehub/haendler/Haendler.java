4fb5c3cdeb2fbe65ebbf1fa1bd40ba5b4fdabdc0
11761910 <tobiaskretschmer@auf.bundeswehr.de>
2023-11-16 12:49:59 +0100

    Haendler zwischenspeicherung

--- FinalFantasyCIII/src/gamehub/haendler/Haendler.java
+++ FinalFantasyCIII/src/gamehub/haendler/Haendler.java
@@ -1,10 +1,36 @@
 package gamehub.haendler;
 
+import gegenstand.Ausruestungsgegenstand.Accesssoire;
+import gegenstand.Ausruestungsgegenstand.Ruestung;
+import gegenstand.Ausruestungsgegenstand.Waffe;
+import gegenstand.Gegenstand;
+import gegenstand.material.Material;
+import gegenstand.verbrauchsgegenstand.Verbrauchsgegenstand;
+
+import java.util.ArrayList;
+
+/**
+ * @author OF Kretschmer
+ * @since 16.11.23
+ */
+
 public class Haendler {
 
-    Waffe[] waffen;
-    Accessoire[] accessoires;
-    Ruestung[] ruestungen;
-    Verbrauchsgegenstand[] verbrauchsgegenstaende;
-    Material[] materialien;
+    private Waffe[] waffen;
+    private Accesssoire[] accessoires;
+    private Ruestung[] ruestungen;
+    private Verbrauchsgegenstand[] verbrauchsgegenstaende;
+    private Material[] materialien;
+    private ArrayList<Gegenstand> zurueckkaufenHistorie;
+
+    public Haendler() {
+
+        this.zurueckkaufenHistorie = new ArrayList<>();
+    }
+
+
+    public ArrayList<Gegenstand> getZurueckkaufenHistorie() {
+        return zurueckkaufenHistorie;
+    }
+
 }
