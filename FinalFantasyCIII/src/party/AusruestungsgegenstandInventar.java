55a9900051de5ff1acf97e17f595dfb559a693e9
11777914 <oliverebert@auf.bundeswehr.de>
2023-11-17 15:05:54 +0100

    AusruestungsgegenstandInventar erweitert und mit Party verknuepft

--- FinalFantasyCIII/src/party/AusruestungsgegenstandInventar.java
+++ FinalFantasyCIII/src/party/AusruestungsgegenstandInventar.java
@@ -1,6 +1,7 @@
 package party;
 
 import charakter.controller.CharakterController;
+import charakter.model.SpielerCharakter;
 import gegenstand.Ausruestungsgegenstand.Accessoire;
 import gegenstand.Ausruestungsgegenstand.Ausruestungsgegenstand;
 import gegenstand.Ausruestungsgegenstand.Ruestungen.Ruestung;
@@ -53,9 +54,36 @@ public class AusruestungsgegenstandInventar {
 
 //statische Methoden
     public static ArrayList<Ausruestungsgegenstand> getGetrageneAusreustungsgegenstaende(Party party){
-        ArrayList<Ausruestungsgegenstand> returnListe = new ArrayList<>();
-        return null;
+        ArrayList<Ausruestungsgegenstand> returnListe = new ArrayList<>(CharakterController.ausruestungAnzeigen(party.getHauptCharakter()));
+        for(SpielerCharakter charakter : party.getNebenCharakter()){
+            returnListe.addAll(CharakterController.ausruestungAnzeigen(charakter));
+        }
+        return returnListe;
     }
 
+//Getter / Setter
 
+    public ArrayList<Waffe> getInventarWaffen() {
+        return inventarWaffen;
+    }
+
+    public void setInventarWaffen(ArrayList<Waffe> inventarWaffen) {
+        this.inventarWaffen = inventarWaffen;
+    }
+
+    public ArrayList<Ruestung> getInventarRuestung() {
+        return inventarRuestung;
+    }
+
+    public void setInventarRuestung(ArrayList<Ruestung> inventarRuestung) {
+        this.inventarRuestung = inventarRuestung;
+    }
+
+    public ArrayList<Accessoire> getInventarAccessiore() {
+        return inventarAccessiore;
+    }
+
+    public void setInventarAccessiore(ArrayList<Accessoire> inventarAccessiore) {
+        this.inventarAccessiore = inventarAccessiore;
+    }
 }
