54c6674513fdb40c07cc8238ed5d9dfaadc9420a
11761910 <tobiaskretschmer@auf.bundeswehr.de>
2023-11-20 12:59:19 +0100

    Accessoire mit Boni ausgestattet

--- FinalFantasyCIII/src/gegenstand/Ausruestungsgegenstand/Accessoire.java
+++ FinalFantasyCIII/src/gegenstand/Ausruestungsgegenstand/Accessoire.java
@@ -3,12 +3,18 @@ package gegenstand.Ausruestungsgegenstand;
 import hilfsklassen.ZufallsZahlenGenerator;
 
 /**
- * @param
  * @author OF Kretschmer
  * @since 17.11.23
  * Objektklasse für die Accessoire
  */
 public class Accessoire extends Ausruestungsgegenstand {
+
+    private int maxGesundheitsPunkte;
+    private int maxManaPunkte;
+    private int gesundheitsRegeneration;
+    private int manaRegeneration;
+    private int beweglichkeit;
+
 
     /**
      * @param stufe
@@ -31,7 +37,7 @@ public class Accessoire extends Ausruestungsgegenstand {
      */
     public Accessoire(int stufe, boolean istNichtKaufbar) {
         super();
-        this.setIstNichtKaufbar(false);
+        this.setIstNichtKaufbar(!istNichtKaufbar);
         int einzigartigeChance = ZufallsZahlenGenerator.zufallsZahlIntAb1(100);
         //Der Wert in der If Prüfung regelt die Dropchance für einzigartige Accessoires
         if (einzigartigeChance > 5) {
@@ -45,7 +51,7 @@ public class Accessoire extends Ausruestungsgegenstand {
     /**
      * @author OF Kretschmer
      * @since 17.11.23
-     * @param stufe
+     * @param stufe des Charakters
      * Erstellt ein normales Accessoire
      */
     private void normalesAccessoire(int stufe) {
@@ -54,20 +60,35 @@ public class Accessoire extends Ausruestungsgegenstand {
         this.setVerkaufswert(stufe * 16);
         this.setLevelAnforderung(ZufallsZahlenGenerator.zufallsZahlIntGegenstandsstufe(stufe));
         this.setIstSoeldnerItem(false);
-        //TODO this.bonus =  CharakterAttribut [ZufallsZahlenGenerator.zufallsZahlIntAb0(4);
+        this.setMaxGesundheitsPunkte(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setMaxManaPunkte(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setGesundheitsRegeneration(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setManaRegeneration(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setBeweglichkeit(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
     }
 
     private void einzigartigesAccessoire(int stufe) {
         int stelle = ZufallsZahlenGenerator.zufallsZahlIntAb0(einzigartigeNamenArray.length);
         this.setName(einzigartigeNamenArray[stelle]);
-            //TODO this.setBonus =  CharakterAttribut [ZufallsZahlenGenerator.zufallsZahlIntAb0(4);
+
         if (this.getName().equals("Kaputter Ring")) {
-            this.setVerkaufswert(stufe * 1);
-            //TODO this.setBonusUmfang = 1;;
+            this.setVerkaufswert(stufe * 2);
+
+            this.setMaxGesundheitsPunkte(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setMaxManaPunkte(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setGesundheitsRegeneration(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setManaRegeneration(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setBeweglichkeit(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+
         } else {
             einzigartigeNamenArray[stelle] = "Kaputter Ring";
             this.setVerkaufswert(stufe * 16);
-            //TODO this.setBonusUmfang = stufe *10;;
+
+            this.setMaxGesundheitsPunkte(stufe * 15);
+            this.setMaxManaPunkte(stufe * 15);
+            this.setGesundheitsRegeneration(stufe * 15);
+            this.setManaRegeneration(stufe * 15);
+            this.setBeweglichkeit(stufe * 15);
         }
         this.setKaufwert(stufe * 20);
         this.setLevelAnforderung(ZufallsZahlenGenerator.zufallsZahlIntGegenstandsstufe(stufe));
@@ -79,12 +100,51 @@ public class Accessoire extends Ausruestungsgegenstand {
             "Diamantring", "Rubinring", "Saphirring", "Silberring", " Smaragdring"};
 
 
-    private static String[] einzigartigeNamenArray = {
-            "Das Herz des Ozeans - Titanic",//Mana
-            "Das Glas voll Dreck",//HP
-            "Das Diadem von Ravenclaw",// Mana
-            "Krone von Barenziah",// HP
-            "Der eine Ring "};//beweglichkeit
+    private static final String[] einzigartigeNamenArray = {
+            "Das Herz des Ozeans - Titanic",
+            "Das Glas voll Dreck",
+            "Das Diadem von Ravenclaw",
+            "Krone von Barenziah",
+            "Der eine Ring "};
 
-    //TODO CharakterAttribut [] = {maxGesundheit,  gesundheitsRegeneration, beweglichkeit,maxMana, manaRegeneration};
+
+    public int getMaxGesundheitsPunkte() {
+        return maxGesundheitsPunkte;
+    }
+
+    public void setMaxGesundheitsPunkte(int maxGesundheitsPunkte) {
+        this.maxGesundheitsPunkte = maxGesundheitsPunkte;
+    }
+
+    public int getMaxManaPunkte() {
+        return maxManaPunkte;
+    }
+
+    public void setMaxManaPunkte(int maxManaPunkte) {
+        this.maxManaPunkte = maxManaPunkte;
+    }
+
+    public int getGesundheitsRegeneration() {
+        return gesundheitsRegeneration;
+    }
+
+    public void setGesundheitsRegeneration(int gesundheitsRegeneration) {
+        this.gesundheitsRegeneration = gesundheitsRegeneration;
+    }
+
+    public int getManaRegeneration() {
+        return manaRegeneration;
+    }
+
+    public void setManaRegeneration(int manaRegeneration) {
+        this.manaRegeneration = manaRegeneration;
+    }
+
+    public int getBeweglichkeit() {
+        return beweglichkeit;
+    }
+
+    public void setBeweglichkeit(int beweglichkeit) {
+        this.beweglichkeit = beweglichkeit;
+    }
 }
