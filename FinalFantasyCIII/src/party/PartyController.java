5aed4bf0ce2b51ecc540c10a711cd5ec0086c083
11761910 <tobiaskretschmer@auf.bundeswehr.de>
2023-11-20 13:43:36 +0100

    Hilfsmethoden zum anzeigen & hinzufügen & entnehmen von Material und Verbrauchsgegenständen

--- FinalFantasyCIII/src/party/PartyController.java
+++ FinalFantasyCIII/src/party/PartyController.java
@@ -4,6 +4,7 @@ import charakter.controller.CharakterController;
 import charakter.model.SpielerCharakter;
 import gegenstand.Ausruestungsgegenstand.Ausruestungsgegenstand;
 import gegenstand.material.Material;
+import gegenstand.verbrauchsgegenstand.Verbrauchsgegenstand;
 
 import java.util.Map;
 
@@ -17,30 +18,31 @@ public class PartyController {
 
     /**
      * gibt das Party-Objekt zurueck
+     *
+     * @return Party
      * @author Nick
      * @since 16.11.2023
-     * @return Party
      */
-    public Party getParty(){
+    public Party getParty() {
         return party;
     }
 
     /**
+     * @return double Partydurchschnittslevel
      * @author Nick
      * @since 16.11.2023
-     * @return double Partydurchschnittslevel
      */
-    public double getPartyLevel(){
-        double partyLevel = party.getHauptCharakter().getLevel()+0.0;
+    public double getPartyLevel() {
+        double partyLevel = party.getHauptCharakter().getLevel() + 0.0;
         int nebencharCounter = 0;
-        for (SpielerCharakter nebenchar : party.getNebenCharakter()){
-            if(nebenchar != null && nebenchar.getGesundheitsPunkte() > 0){
+        for (SpielerCharakter nebenchar : party.getNebenCharakter()) {
+            if (nebenchar != null && nebenchar.getGesundheitsPunkte() > 0) {
                 partyLevel += (nebenchar.getLevel() + 0.0);
                 nebencharCounter++;
             }
         }
-        if(nebencharCounter != 0){
-        return (partyLevel/nebencharCounter);
+        if (nebencharCounter != 0) {
+            return (partyLevel / nebencharCounter);
         } else {
             return partyLevel;
         }
@@ -48,37 +50,39 @@ public class PartyController {
 
     /**
      * Fuegt dem Partyinventar Gold hinzu
+     *
+     * @param hinzuzufuegendesGold
      * @author Nick
      * @since 16.11.2023
-     * @param hinzuzufuegendesGold
      */
-    public void goldHinzufuegen(int hinzuzufuegendesGold){
+    public void goldHinzufuegen(int hinzuzufuegendesGold) {
         party.setGold(party.getGold() + hinzuzufuegendesGold);
     }
 
     /**
+     * @return int Partygold
      * @author Nick
      * @since 16.11.2023
-     * @return int Partygold
      */
-    public int getPartyGold(){
+    public int getPartyGold() {
         return party.getGold();
     }
 
     /**
      * DIESE METHODE PRÜFT NICHT OB GENUG GOLD VORHANDEN IST!
+     *
+     * @param abzuziehendesGold
      * @author Nick
      * @since 16.11.2023
-     * @param abzuziehendesGold
      */
-    public void goldAbziehen(int abzuziehendesGold){
+    public void goldAbziehen(int abzuziehendesGold) {
         party.setGold(party.getGold() - abzuziehendesGold);
     }
 
-    public void teammitgliedHinzufuegen(SpielerCharakter spielerCharakter){
+    public void teammitgliedHinzufuegen(SpielerCharakter spielerCharakter) {
         SpielerCharakter[] nebenCharaktere = party.getNebenCharakter();
         if (nebenCharaktere[0] == null) {
-        nebenCharaktere[0] = spielerCharakter;
+            nebenCharaktere[0] = spielerCharakter;
         } else {
             if (nebenCharaktere[1] == null) {
                 nebenCharaktere[1] = spielerCharakter;
@@ -106,16 +110,16 @@ public class PartyController {
         }
     }
 
-    public void ausruestungsgegenstandHinzufuegen(Ausruestungsgegenstand ausruestungsgegenstand){
+    public void ausruestungsgegenstandHinzufuegen(Ausruestungsgegenstand ausruestungsgegenstand) {
         AusruestungsgegenstandInventar ausruestungen = party.getAusruestungsgegenstandInventar();
-        if(!ausruestungen.getGesamteAusruestungsgegenstaende().contains(ausruestungsgegenstand)){
+        if (!ausruestungen.getGesamteAusruestungsgegenstaende().contains(ausruestungsgegenstand)) {
             ausruestungen.ausruestungsgegenstandHinzufuegen(ausruestungsgegenstand);
         } else {
             System.err.println("PartyController.ausruestungsgegenstandHinzufuegen(): Gegenstand ist bereits im AusreustungsgegenstandInventar");
         }
     }
 
-    public void ausruestungsgegenstandEntfernen(Ausruestungsgegenstand ausruestungsgegenstand){
+    public void ausruestungsgegenstandEntfernen(Ausruestungsgegenstand ausruestungsgegenstand) {
         AusruestungsgegenstandInventar ausruestungsgegenstandInventar = party.getAusruestungsgegenstandInventar();
         ausruestungsgegenstandInventar.ausruestungsgegenstandEntfernen(ausruestungsgegenstand);
     }
@@ -128,12 +132,93 @@ public class PartyController {
      * @author Nick
      * @since 20.11.2023
      */
-    public void materialHinzufuegen(Material mat, int anzahl){
+    public void materialHinzufuegen(Material mat, int anzahl) {
         Map<Material, Integer> matInventar = party.getMaterialien();
-        for(Map.Entry<Material, Integer> entry : matInventar.entrySet()){
-            if(entry.getClass().getSimpleName().equals(mat.getClass().getSimpleName())){
+        for (Map.Entry<Material, Integer> entry : matInventar.entrySet()) {
+            if (entry.getClass().getSimpleName().equals(mat.getClass().getSimpleName())) {
                 matInventar.put(entry.getKey(), (entry.getValue() + anzahl));
             }
         }
     }
+
+    /**
+     * @param mat
+     * @param anzahl
+     * @author OF Kretschmer
+     * @since 20.11.23
+     * <p>
+     * Entnimmt vom übergebenen Material die übergebene Anzahl
+     */
+    public void materialEntnehmen(Material mat, int anzahl) {
+        Map<Material, Integer> matInventar = party.getMaterialien();
+        for (Map.Entry<Material, Integer> entry : matInventar.entrySet()) {
+            if (entry.getClass().getSimpleName().equals(mat.getClass().getSimpleName())) {
+                matInventar.put(entry.getKey(), (entry.getValue() - anzahl));
+            }
+        }
+    }
+
+    /**
+     * @author OF Kretschmer
+     * @since 20.11.23
+     * <p>
+     * Gibt die vorhandenen Materialien mit ihrer Anzahl aus
+     */
+    public void materialienAusgeben() {
+        Map<Material, Integer> matInventar = party.getMaterialien();
+        for (Map.Entry<Material, Integer> entry : matInventar.entrySet()) {
+            System.out.printf("%5d x  %s", entry.getValue(), entry.getKey().toString());
+        }
+    }
+
+    /**
+     * @param verbrauchsgegenstand
+     * @param anzahl
+     * @author OF Kretschmer
+     * @since 20.11.23
+     * <p>
+     * Fügt dem Inventar vom übergebenen Verbrauchsgegenstand die übergebene Anzahl hinzu
+     */
+    public void verbrauchsgegenstandHinzufuegen(Verbrauchsgegenstand verbrauchsgegenstand, int anzahl) {
+        Map<Verbrauchsgegenstand, Integer> verbrauchsgegenstandInventar = party.getVerbrauchsgegenstaende();
+        for (Map.Entry<Verbrauchsgegenstand, Integer> entry : verbrauchsgegenstandInventar.entrySet()) {
+            if (entry.getClass().getSimpleName().equals(verbrauchsgegenstand.getClass().getSimpleName())) {
+                verbrauchsgegenstandInventar.put(entry.getKey(), (entry.getValue() + anzahl));
+            }
+        }
+    }
+
+
+    /**
+     * @param verbrauchsgegenstand
+     * @param anzahl
+     * @author OF Kretschmer
+     * @since 20.11.23
+     * <p>
+     * Entnimmt vom übergebenen Verbrauchsgegenstand die übergebene Anzahl
+     */
+    public void verbrauchsgegenstandEntnehmen(Verbrauchsgegenstand verbrauchsgegenstand, int anzahl) {
+        Map<Verbrauchsgegenstand, Integer> verbrauchsgegenstandInventar = party.getVerbrauchsgegenstaende();
+        for (Map.Entry<Verbrauchsgegenstand, Integer> entry : verbrauchsgegenstandInventar.entrySet()) {
+            if (entry.getClass().getSimpleName().equals(verbrauchsgegenstand.getClass().getSimpleName())) {
+                verbrauchsgegenstandInventar.put(entry.getKey(), (entry.getValue() - anzahl));
+            }
+        }
+    }
+
+    /**
+     * @author OF Kretschmer
+     * @since 20.11.23
+     * <p>
+     * Gibt die vorhandenen Verbrauchsgegenstände mit ihrer Anzahl aus
+     */
+    public void verbrauchsgegenstaendeAusgeben() {
+        Map<Verbrauchsgegenstand, Integer> verbrauchsgegenstandInventar = party.getVerbrauchsgegenstaende();
+        for (Map.Entry<Verbrauchsgegenstand, Integer> entry : verbrauchsgegenstandInventar.entrySet()) {
+            System.out.printf("%5d x  %s", entry.getValue(), entry.getKey().toString());
+        }
+    }
 }
+
+
+
