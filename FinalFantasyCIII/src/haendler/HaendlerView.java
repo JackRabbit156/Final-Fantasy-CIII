a7380026b3069c3df269ec78a7b3a03371ee87b1
11761910 <tobiaskretschmer@auf.bundeswehr.de>
2023-11-30 14:39:03 +0100

    anpassung

--- FinalFantasyCIII/src/haendler/HaendlerView.java
+++ FinalFantasyCIII/src/haendler/HaendlerView.java
@@ -1,11 +1,14 @@
 package haendler;
 
+import gegenstand.Ausruestungsgegenstand.Waffen.Waffe;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
 
 public class HaendlerView extends TabPane {
 
     TabPane root = new TabPane();
 
+    TableView<Waffe> waffen = new TableView<Waffe>();
 
 
 
