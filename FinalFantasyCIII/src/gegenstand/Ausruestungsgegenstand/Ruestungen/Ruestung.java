967da955ad77a8ea377317ed53cddcbf5c42977d
11777914 <oliverebert@auf.bundeswehr.de>
2023-11-30 14:52:59 +0100

    Neue Attribute für Waffen und Rüstungen

--- FinalFantasyCIII/src/gegenstand/Ausruestungsgegenstand/Ruestungen/Ruestung.java
+++ FinalFantasyCIII/src/gegenstand/Ausruestungsgegenstand/Ruestungen/Ruestung.java
@@ -6,6 +6,9 @@ public class Ruestung extends Ausruestungsgegenstand {
 
     private int verteidigung;
     private int magischeVerteidigung;
+    private int resistenz;
+    private int maxGesundheitsPunkte;
+    private int maxManaPunkte;
 
     public int getVerteidigung() {
         return verteidigung;
@@ -21,5 +24,29 @@ public class Ruestung extends Ausruestungsgegenstand {
 
     public void setMagischeVerteidigung(int magischeVerteidigung) {
         this.magischeVerteidigung = magischeVerteidigung;
+    }
+
+    public int getResistenz() {
+        return resistenz;
+    }
+
+    public void setResistenz(int resistenz) {
+        this.resistenz = resistenz;
+    }
+
+    public int getMaxGesundheitsPunkte() {
+        return maxGesundheitsPunkte;
+    }
+
+    public void setMaxGesundheitsPunkte(int maxGesundheitsPunkte) {
+        this.maxGesundheitsPunkte = maxGesundheitsPunkte;
+    }
+
+    public int getMaxManaPunkte() {
+        return maxManaPunkte;
+    }
+
+    public void setMaxManaPunkte(int maxManaPunkte) {
+        this.maxManaPunkte = maxManaPunkte;
     }
 }
