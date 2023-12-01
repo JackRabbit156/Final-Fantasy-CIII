46f09ebe9413e0036db5f414fd5b001ba75c89eb
11777914 <oliverebert@auf.bundeswehr.de>
2023-12-01 08:17:23 +0100

    umbenennung Soeldner

--- FinalFantasyCIII/src/taverne/TaverneController.java
+++ FinalFantasyCIII/src/taverne/TaverneController.java
@@ -158,7 +158,7 @@ public class TaverneController {
          */
 
         for (int i = 0; i < 3; i++) {
-            generierteSoeldner[i] = generiereEinenZufaelligenSoeldner((int)partyController.getPartyLevel());
+            soeldner[i] = generiereEinenZufaelligenSoeldner((int)partyController.getPartyLevel());
         }
         letzteGeneration = statistikController.getStatistik().getDurchgefuehrteKaempfe();
     }
