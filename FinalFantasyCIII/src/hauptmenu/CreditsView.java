24b41d6196715520a7381503cee09293fd165118
TangoMike <Thomas1Maass@Bundeswehr.org>
2023-12-02 15:52:54 +0100

    Creddits angelegt !

---
+++ FinalFantasyCIII/src/hauptmenu/CreditsView.java
@@ -0,0 +1,9 @@
+package hauptmenu;
+
+import javafx.scene.layout.BorderPane;
+import view.ViewController;
+
+public class CreditsView extends BorderPane {
+    ViewController viewController;
+    //Hier kommen irgendwann die Credits hin !
+}
