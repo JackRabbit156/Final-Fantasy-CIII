2f6b3e7baebedb9fcd1ef3279a569e7ad29b95bd
11436766 <nielstorbenrode@auf.bundeswehr.de>
2023-12-04 15:34:24 +0100

    Hinzugefügte schmiede mit view controller und inventar controller

--- FinalFantasyCIII/src/inventar/InventarController.java
+++ FinalFantasyCIII/src/inventar/InventarController.java
@@ -2,15 +2,18 @@ package inventar;
 
 import charakter.model.SpielerCharakter;
 import party.PartyController;
+import view.ViewController;
 
 import java.util.ArrayList;
 
 public class InventarController {
-    private final PartyController partyController;
+    private PartyController partyController;
+    private ViewController viewController;
     private ArrayList<SpielerCharakter> aktiveParty;
     private SpielerCharakter ausgewaehlterChar;
 
-    public InventarController(PartyController partyController) {
+    public InventarController(PartyController partyController, ViewController viewController) {
+        this.viewController = viewController;
         this.partyController = partyController;
         this.aktiveParty = fuellePartyList();
     }
