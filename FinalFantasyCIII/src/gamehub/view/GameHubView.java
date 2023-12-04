8de64f2be72708f0f8adf8edfcf5acc3a7708d6d
11436766 <nielstorbenrode@auf.bundeswehr.de>
2023-12-04 15:24:07 +0100

    Fertiges Gamehub frfrfrfrfr

--- FinalFantasyCIII/src/gamehub/view/GameHubView.java
+++ FinalFantasyCIII/src/gamehub/view/GameHubView.java
@@ -107,8 +107,8 @@ public class GameHubView extends Pane {
 
         //Infos zur Platzierung für alle anderen, ein bild wird immer gesetzt von der oberen Ecke des Bildes 0,0 vond ort wird es gerender auf den hintergrund
 
-        btnViewPartyInventarEgg.setLayoutX(506);
-        btnViewPartyInventarEgg.setLayoutY(601);
+        btnViewPartyInventarEgg.setLayoutX(922);
+        btnViewPartyInventarEgg.setLayoutY(522);
 
 
         btnViewPartyInventarEggHSC.setLayoutX(15);
