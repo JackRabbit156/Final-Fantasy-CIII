5af462f8011998305bfe36539b739bf3d1353ca5
11750396 <11750396@auf.bundeswehr.de>
2023-12-04 08:06:35 +0100

    Partystatus angepasst

--- FinalFantasyCIII/src/partystatus/PartyStatusController.java
+++ FinalFantasyCIII/src/partystatus/PartyStatusController.java
@@ -1,27 +1,12 @@
 package partystatus;
 
-import charakter.controller.CharakterController;
 import charakter.model.SpielerCharakter;
-import charakter.model.klassen.soeldner.Supporter;
-import gegenstand.Ausruestungsgegenstand.Accessoire;
-import gegenstand.Ausruestungsgegenstand.Ausruestungsgegenstand;
-import gegenstand.Ausruestungsgegenstand.Ruestungen.Ruestung;
-import gegenstand.Ausruestungsgegenstand.Waffen.Waffe;
-import gegenstand.Gegenstand;
-import gegenstand.verbrauchsgegenstand.Verbrauchsgegenstand;
-import hilfsklassen.Farbauswahl;
-import hilfsklassen.KonsolenAssistent;
-import hilfsklassen.ScannerHelfer;
 import javafx.scene.control.Button;
-import party.AusruestungsgegenstandInventar;
 import party.PartyController;
 import view.AnsichtsTyp;
 import view.ViewController;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.function.ToIntFunction;
-import java.util.stream.IntStream;
 
 public class PartyStatusController {
     private final PartyController partyController;
@@ -46,7 +31,8 @@ public class PartyStatusController {
         partyStatusView.anzeigeAktualiseren();
         viewController.anmelden(partyStatusView, buttons, AnsichtsTyp.MIT_OVERLAY);
     }
-    public SpielerCharakter[] getPartyMitglieder(){
+
+    public SpielerCharakter[] getPartyMitglieder() {
         return partyController.getTeammitglieder();
     }
 
