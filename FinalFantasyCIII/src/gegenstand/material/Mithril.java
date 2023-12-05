2958310658d0d2e8c1acea28ee637a954dc0be54
11750396 <11750396@auf.bundeswehr.de>
2023-12-05 17:16:03 +0100

    Mithril

--- FinalFantasyCIII/src/gegenstand/material/Mithril.java
+++ FinalFantasyCIII/src/gegenstand/material/Mithril.java
@@ -3,7 +3,7 @@ package gegenstand.material;
 public class Mithril extends Material{
 
     public Mithril() {
-        this.setName("Mitrhil");
+        this.setName("Mithril");
         this.setKaufwert(150);
         this.setVerkaufswert(75);
         this.setIcon("/icons/mithril.png");
