1d24444d5317ef1ef8fe2b2360c09bad9b256794
11761910 <tobiaskretschmer@auf.bundeswehr.de>
2023-12-06 13:13:42 +0100

    Bug fix

--- FinalFantasyCIII/src/gegenstand/Ausruestungsgegenstand/Accessoire.java
+++ FinalFantasyCIII/src/gegenstand/Ausruestungsgegenstand/Accessoire.java
@@ -24,13 +24,13 @@ public class Accessoire extends Ausruestungsgegenstand {
      *
      * @param stufe -
      * @author OF Kretschmer
-     * @since 17.11.23
+     * @since 05.12.23
      */
     public Accessoire(int stufe) {
         super();
-        normalesAccessoire(stufe);
         this.setIstNichtKaufbar(true);
         this.setLevelAnforderung(ZufallsZahlenGenerator.zufallsZahlIntGegenstandsstufe(stufe));
+        normalesAccessoire(stufe,this.getLevelAnforderung());
     }
 
     /**
@@ -39,7 +39,7 @@ public class Accessoire extends Ausruestungsgegenstand {
      * @param stufe           -
      * @param istNichtKaufbar -
      * @author OF Kretschmer
-     * @since 17.11.23
+     * @since 05.12.23
      */
     public Accessoire(int stufe, boolean istNichtKaufbar) {
         super();
@@ -48,9 +48,9 @@ public class Accessoire extends Ausruestungsgegenstand {
         int einzigartigeChance = ZufallsZahlenGenerator.zufallsZahlIntAb1(100);
         //Der Wert in der If Prüfung regelt die Dropchance für einzigartige Accessoires
         if (einzigartigeChance > 5) {
-            normalesAccessoire(stufe);
+            normalesAccessoire(stufe, this.getLevelAnforderung());
         } else {
-            einzigartigesAccessoire(stufe);
+            einzigartigesAccessoire(stufe, this.getLevelAnforderung());
         }
 
     }
@@ -62,17 +62,17 @@ public class Accessoire extends Ausruestungsgegenstand {
      * @author OF Kretschmer
      * @since 30.11.23
      */
-    private void normalesAccessoire(int stufe) {
+    private void normalesAccessoire(int stufe, int lvlAnforderung) {
         this.setIcon("icons/AccNormal.png");
         this.setName(namenArray[ZufallsZahlenGenerator.zufallsZahlIntAb0(namenArray.length-1)]);
-        this.setKaufwert(stufe * 20);
-        this.setVerkaufswert(stufe * 16);
+        this.setKaufwert(lvlAnforderung * 20);
+        this.setVerkaufswert(lvlAnforderung * 16);
         this.setIstSoeldnerItem(false);
-        this.setMaxGesundheitsPunkte(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
-        this.setMaxManaPunkte(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
-        this.setGesundheitsRegeneration(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
-        this.setManaRegeneration(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
-        this.setBeweglichkeit(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setMaxGesundheitsPunkte(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setMaxManaPunkte(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setGesundheitsRegeneration(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setManaRegeneration(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
+        this.setBeweglichkeit(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(10)));
     }
 
     /**
@@ -83,29 +83,29 @@ public class Accessoire extends Ausruestungsgegenstand {
      * @author OF Kretschmer
      * @since 30.11.23
      */
-    private void einzigartigesAccessoire(int stufe) {
+    private void einzigartigesAccessoire(int stufe, int lvlAnforderung) {
         int stelle = ZufallsZahlenGenerator.zufallsZahlIntAb0(einzigartigeNamenArray.length-1);
         this.setName(einzigartigeNamenArray[stelle]);
         if (this.getName().equals("Kaputter Ring")) {
             this.setIcon("icons/AccKaputt.png");
             this.setVerkaufswert(stufe * 2);
-            this.setMaxGesundheitsPunkte(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
-            this.setMaxManaPunkte(stufe* (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
-            this.setGesundheitsRegeneration(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
-            this.setManaRegeneration(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
-            this.setBeweglichkeit(stufe * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setMaxGesundheitsPunkte(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setMaxManaPunkte(lvlAnforderung* (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setGesundheitsRegeneration(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setManaRegeneration(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
+            this.setBeweglichkeit(lvlAnforderung * (ZufallsZahlenGenerator.zufallsZahlIntAb0(2)));
 
         } else {
             einzigartigeNamenArray[stelle] = "Kaputter Ring";
             this.setIcon("icons/AccEinzigartig.png");
-            this.setVerkaufswert(stufe * 25);
-            this.setMaxGesundheitsPunkte(stufe * 15);
-            this.setMaxManaPunkte(stufe * 15);
-            this.setGesundheitsRegeneration(stufe * 15);
-            this.setManaRegeneration(stufe * 15);
-            this.setBeweglichkeit(stufe* 25);
+            this.setVerkaufswert(lvlAnforderung * 25);
+            this.setMaxGesundheitsPunkte(lvlAnforderung * 25);
+            this.setMaxManaPunkte(lvlAnforderung * 25);
+            this.setGesundheitsRegeneration(lvlAnforderung * 25);
+            this.setManaRegeneration(lvlAnforderung * 25);
+            this.setBeweglichkeit(lvlAnforderung* 25);
         }
-        this.setKaufwert(stufe * 25);
+        this.setKaufwert(lvlAnforderung * 25);
         this.setIstSoeldnerItem(false);
     }
 
