8d03b429ddc5edcdf7022c501409bd1f2a8593a6
Frank Rieger <FrankRieger@auf.bundeswehr.org>
2024-01-19 09:10:04 +0100

    Gleiche Benennung korrigiert

--- FinalFantasyCIII/src/de/bundeswehr/auf/final_fantasy/charakter/controller/FeindController.java
+++ FinalFantasyCIII/src/de/bundeswehr/auf/final_fantasy/charakter/controller/FeindController.java
@@ -71,64 +71,79 @@ public class FeindController {
     public Feind[] gegnerGenerieren(PartyController partyController) {
         int feindAnzahl = ZufallsZahlenGenerator.zufallsZahlAb1Inklusive(4);
         int partyLevel = (int) partyController.getPartyLevel();
-        Feind[] feindlisteReturn = new Feind[feindAnzahl];
-        for (int i = 0; i < feindlisteReturn.length; i++) {
+        Feind[] feinde = new Feind[feindAnzahl];
+        for (int i = 0; i < feinde.length; i++) {
             int feindLevel = getFeindLevel(partyLevel + ZufallsZahlenGenerator.zufallsZahlAb0Inklusive(4) - feindAnzahl);
             switch (ZufallsZahlenGenerator.zufallsZahlAb0(16)) {
                 case 0:
-                    feindlisteReturn[i] = new VerueckterDoktor(feindLevel);
+                    feinde[i] = new VerueckterDoktor(feindLevel);
                     break;
                 case 1:
-                    feindlisteReturn[i] = new Suzaku(feindLevel);
+                    feinde[i] = new Suzaku(feindLevel);
                     break;
                 case 2:
-                    feindlisteReturn[i] = new GefallenerEngel(feindLevel);
+                    feinde[i] = new GefallenerEngel(feindLevel);
                     break;
                 case 3:
-                    feindlisteReturn[i] = new Mecha(feindLevel);
+                    feinde[i] = new Mecha(feindLevel);
                     break;
                 case 4:
-                    feindlisteReturn[i] = new SahuaginKampfMagier(feindLevel);
+                    feinde[i] = new SahuaginKampfMagier(feindLevel);
                     break;
                 case 5:
-                    feindlisteReturn[i] = new SahuaginKrieger(feindLevel);
+                    feinde[i] = new SahuaginKrieger(feindLevel);
                     break;
                 case 6:
-                    feindlisteReturn[i] = new Krankenschwester(feindLevel);
+                    feinde[i] = new Krankenschwester(feindLevel);
                     break;
                 case 7:
-                    feindlisteReturn[i] = new Genbu(feindLevel);
+                    feinde[i] = new Genbu(feindLevel);
                     break;
                 case 8:
-                    feindlisteReturn[i] = new GothicLolita(feindLevel);
+                    feinde[i] = new GothicLolita(feindLevel);
                     break;
                 case 9:
-                    feindlisteReturn[i] = new Inugami(feindLevel);
+                    feinde[i] = new Inugami(feindLevel);
                     break;
                 case 10:
-                    feindlisteReturn[i] = new Tengu(feindLevel);
+                    feinde[i] = new Tengu(feindLevel);
                     break;
                 case 11:
-                    feindlisteReturn[i] = new Seiryu(feindLevel);
+                    feinde[i] = new Seiryu(feindLevel);
                     break;
                 case 12:
-                    feindlisteReturn[i] = new KillerRoboter(feindLevel);
+                    feinde[i] = new KillerRoboter(feindLevel);
                     break;
                 case 13:
-                    feindlisteReturn[i] = new UntoterSamurai(feindLevel);
+                    feinde[i] = new UntoterSamurai(feindLevel);
                     break;
                 case 14:
-                    feindlisteReturn[i] = new SahuaginTank(feindLevel);
+                    feinde[i] = new SahuaginTank(feindLevel);
                     break;
                 case 15:
-                    feindlisteReturn[i] = new Schlaechter(feindLevel);
+                    feinde[i] = new Schlaechter(feindLevel);
             }
         }
-        return feindlisteReturn;
+        updateNamen(feinde);
+        return feinde;
     }
 
     private int getFeindLevel(int feindlevel) {
         return feindlevel <= 0 ? 1 : feindlevel;
+    }
+
+    private void updateNamen(Feind[] feinde) {
+        for (int i = 0; i < feinde.length; i++) {
+            String name = feinde[i].getName();
+            StringBuilder zusatz = new StringBuilder(" II");
+            for (int ii = i + 1; ii < feinde.length; ii++) {
+                if (name.equals(feinde[ii].getName())) {
+                    feinde[i].setName(name + " I");
+                    feinde[ii].setName(feinde[ii].getName() + zusatz);
+                    zusatz.append("I");
+                }
+            }
+        }
     }
 
 }
